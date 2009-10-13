4b414af5472ea544deaed3d7b54bec512552b4e2
f.canas.s <f.canas.s@7d5d1d20-8da2-11de-849c-55eda45439ce>
2009-10-13 07:19:34 +0000

    * Modificado Makefile * Agregado comienzo de PopMessage

--- trunk/src/Message.cpp
+++ trunk/src/Message.cpp
@@ -1,31 +1,11 @@
 /*
- * Message.cpp
- *
  *  Created on: Sep 25, 2009
  *      Author: psepulv
+ *
+ * Últimos cambios:
+ * $Author$
+ * $Rev$
  */
 
 #include "Message.h"
 
-Message::Message() {
-	// TODO Auto-generated constructor stub
-
-}
-
-bool Message::isReady()
-{
-	return true;
-}
-
-Message::Message(int ip_server, short  port_server, int ip_client, short  port_client)
-{
-}
-
-void Message::append(char msg[], size_t l)
-{
-}
-
-
-Message::~Message() {
-	// TODO Auto-generated destructor stub
-}
---
+++ trunk/src/pop/PopMessage.cpp
@@ -0,0 +1,16 @@
+#include "PopMessage.h"
+
+void PopMessage::append (char* data, size_t len)
+{
+}
+
+bool PopMessage::isReady () const
+{
+    return true;
+}
+
+bool PopMessage::isFromServer () const
+{
+    return true;
+}
+
